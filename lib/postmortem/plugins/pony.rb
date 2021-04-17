829f84af0dee680fb676fca1b2349db553ec699c
Bob Farrell <git@bob.frl>
2021-04-17 18:52:33 +0100

    Call original `Pony.mail` method when delivery method is `test`
    `ActionMailer.deliveries` delegates to `Mail::TestMailer.deliveries` so,
    when `via` is `test` we do not want to block delivery as this will break
    any test/development code that inspects `ActionMailer.deliveries`.

--- lib/postmortem/plugins/pony.rb
+++ lib/postmortem/plugins/pony.rb
@@ -6,10 +6,14 @@ module Pony
     alias _original_mail mail
 
     def mail(options)
-      # SMTP delivery is handled by Mail plugin further down the stack
-      return _original_mail(options) if options[:via].to_s == 'smtp'
+      strategy = options[:via].to_s
+      # Pony uses the Mail gem for smtp delivery so we catch these further down the stack to
+      # avoid duplicating deliveries.
+      return _original_mail(options) if strategy == 'smtp'
 
-      result = _original_mail(options) unless Postmortem.config.mail_skip_delivery
+      # When delivery method is "test" we do not want to interfere as ActionMailer.deliveries
+      # (which delegates to Mail::TestMailer) is typically used in tests.
+      result = _original_mail(options) if strategy == 'test' || !Postmortem.config.mail_skip_delivery
       Postmortem.record_delivery(Postmortem::Adapters::Pony.new(options))
       result
     end
